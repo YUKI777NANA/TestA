9b42f4328bb8d795422e1d78ee952abda812bfad
Yuki <fujishima724@gmail.com>
2022-12-04 09:36:11 +0900

    Initial Commit

---
+++ TestA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestA
+//
+//  Created by 藤島雄基 on 2022/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
